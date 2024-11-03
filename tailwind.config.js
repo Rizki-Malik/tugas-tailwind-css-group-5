4fb9ddb117307fad80241fe015c9fa5656e33d4d
Ky0ma <rimali.qwerty@gmail.com>
2024-11-03 13:45:44 +0700

    Rizki Malik : Admin dan profile

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./assets/**/*.{js,jsx,ts,tsx}",
+    "./pages/**/*.{js,html}",
+    "./index.html",
+    "./register.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
